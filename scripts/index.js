665fe6cd0a5f79cbeb170b8a3ae8fe2c34d97b12
Alina Shebalkina <farmazonka1994@yandex.ru>
2022-03-27 02:06:32 +0300

    правки

--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,30 @@ let popup = document.querySelector('.popup'); //объект попапа
 let buttonEdit = document.querySelector('.profile__edit'); //кнопка редактирования
 let buttonClose = document.querySelector('.popup__close'); //кнопка закрытия попапа
 let formElement = document.querySelector('.form'); //форма
-let nameInput = formElement.querySelector('.form__item_name'); //ввод имени
-let jobInput = formElement.querySelector('.form__item_job'); //ввод работы
+let nameInput = formElement.querySelector('.form__item_type_name'); //ввод имени
+let jobInput = formElement.querySelector('.form__item_type_job'); //ввод работы
 let nameProfile = document.querySelector('.profile__name'); // имя
 let jobProfile = document.querySelector('.profile__job'); //работа
 
 
-buttonEdit.addEventListener('click', function () {
+function openPopup() {
   popup.classList.add('popup_opened');
-});
+}
 
-buttonClose.addEventListener('click', function () {
+buttonEdit.addEventListener('click', openPopup);
+
+function closedPopup() {
   popup.classList.remove('popup_opened');
-});
+}
+
+buttonClose.addEventListener('click', closedPopup);
 
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  popup.classList.remove('popup_opened');
+  closedPopup();
 };
 
 formElement.addEventListener('submit', formSubmitHandler);
